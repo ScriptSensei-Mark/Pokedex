fe31859ff61a454b5acd7c1597dc6c9d5f5c21dd
Mark Waclawec <m.s.waxlawex.86@gmail.com>
2023-11-13 06:11:09 +0100

    minor changes

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const MAIN_URL = 'https://pokeapi.co/api/v2/pokemon?limit=51';
+const MAIN_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 async function init() {
-    await fetchData();
+    await renderPokemons();
 };
 
 async function fetchData() {
     let url = MAIN_URL;
     let response = await fetch(url);
-    let firstBatch = await response.json();
+    let responseJSON = await response.json();
+    return responseJSON;
+};
+
+
+async function renderPokemons() {
+    await fetchData
 };
